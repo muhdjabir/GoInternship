e86a24517e084c3060b7619aa3f198f0a4e9b9b8
muhdjabir <e0725438@u.nus.edu>
2023-06-30 11:52:58 +0800

    Implemented placeholder user auth routes

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"backend/controllers"
 	"backend/database"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +13,10 @@ func main() {
 	database.Connect()
 	router := gin.Default()
 	fmt.Println("Server is listening at PORT:8080")
-	router.GET("/", GetUser)
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome to GoInternship Golang Web App"})
+	})
+	router.POST("/register", controllers.Register)
+	router.POST("/login", controllers.Login)
 	router.Run("0.0.0.0:8080")
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -7,5 +7,6 @@ import (
 type User struct {
 	gorm.Model
 	Username string `gorm:"size:255;not null;unique" json:"username"`
+	Email    string `gorm:"size:255;not null;unique" json:"email"`
 	Password string `gorm:"size:255,not null;" json:"password"`
 }
